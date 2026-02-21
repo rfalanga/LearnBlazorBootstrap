560d11de957c6767b484b358ba1f980b4a1201cf
Rod Falanga <rfalanga@users.noreply.github.com>
2026-02-21 07:17:05 -0700

    Added a People collection with 3 items

---
+++ LearnBlazorBootstrap/LearnBlazorBootstrap/Components/Pages/Home.razor.cs
@@ -0,0 +1,18 @@
+﻿using LearnBlazorBootstrap.Models;
+
+namespace LearnBlazorBootstrap.Components.Pages;
+
+public partial class Home
+{
+    private List<Person> People { get; set; } = new List<Person>();
+
+    protected override void OnInitialized()
+    {
+        People = new List<Person>
+        {
+            new Person { Id = 1, FirstName = "John", LastName = "Doe", Age = 30 },
+            new Person { Id = 2, FirstName = "Jane", LastName = "Smith", Age = 25 },
+            new Person { Id = 3, FirstName = "Bob", LastName = "Johnson", Age = 40 }
+        };
+    }
+}
