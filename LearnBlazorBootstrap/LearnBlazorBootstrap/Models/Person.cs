b0a20176975ae7fb8c33c35588b2e35a3c0055a1
Rod Falanga <rfalanga@users.noreply.github.com>
2026-02-20 18:26:25 -0700

    Added Person class

---
+++ LearnBlazorBootstrap/LearnBlazorBootstrap/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnBlazorBootstrap.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Age { get; set; } = 0;
+}
